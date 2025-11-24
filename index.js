6e0896c12dc5469d973ed6a6929328dee2d5b510
khushbooverma2197 <khushbooverma2197@gmail.com>
2025-11-24 22:53:16 +0530

    Merge profile and updates into mergedProfile

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const multiply = (a = 1, b = 1) => a * b;
-
-console.log(multiply(3, 4)); //12
-console.log(multiply()); //1
+const profile = {
+ name: "Charlie",
+ age: 29,
+ address: {
+   city: "San Francisco",
+   zipcode: "94101"
+ }
+};
+const updates = {
+ age: 30,
+ address: {
+   zipcode: "94109",
+   country: "USA"
+ }
+};
+const mergedProfile = {
+ ...profile,
+ ...updates,
+ address: {
+   ...profile.address,
+   ...updates.address
+ }
+};
+console.log(mergedProfile);
