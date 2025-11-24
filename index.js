b41ec51e880eb6ae2576ac6903202ccf38e3c387
khushbooverma2197 <khushbooverma2197@gmail.com>
2025-11-24 23:01:24 +0530

    Enhance index.js with user and course details
    Added user and course information, created a student object with a greet method, and implemented a function to get full name.

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-const multiply = (a = 1, b = 1) => a * b;
+const username = "Sam";
 
-console.log(multiply(3, 4)); //12
-console.log(multiply()); //1
+const course = "JavaScript";
+
+console.log(`Hello, welcome to the course ${course}, ${username}!`);
+ 
+const name = "Sam";
+
+const age = 21;
+
+const student = {
+
+  name: name,
+
+  age: age,
+
+  greet: function () {
+
+    console.log("Hello");
+
+  }
+
+};
+
+console.log(student);
+student.greet();
+ 
+const getFullName = (first, last) => `${first} ${last}`;
+console.log(getFullName("Sam", "Smith"));
+ 
